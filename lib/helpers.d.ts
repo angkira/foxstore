22682a46668fe3354e5c85fc1115a61c043b9285
Angkira <angkira@yandex.ru>
2021-11-23 13:14:11 +0300

    release v3.0.4

--- lib/helpers.d.ts
+++ lib/helpers.d.ts
@@ -3,6 +3,7 @@ import { MaybeAsync } from './core/types';
 export declare type Class<T> = {
     new (...args: any[]): T;
 };
+export declare const writeAs: <State>(path: string) => (payload: unknown, state: State) => Partial<State>;
 export declare const handleStreamOnce: <T>(observer: Partial<Observer<T>>) => (stream$: Observable<T>) => Subscription;
 export declare const applyCallbackToMaybeAsync: <Entity, Result>(fn: (agr: Entity) => Result) => (entity: MaybeAsync<Entity>) => Subscription | Result | Promise<Result>;
 declare type MapObject = <T extends object, K extends keyof T = keyof T, V extends T[K] = T[K], NV = V>(fn: (value: V, key?: K, obj?: T) => NV, obj: T) => Record<K, NV | V>;
