9821fb423c9f3dd73f5c6afe63d53ff7d3d09622
Angkira <angkira@yandex.ru>
2022-01-17 02:02:37 +0300

    release v3.0.10

--- lib/helpers.d.ts
+++ lib/helpers.d.ts
@@ -8,4 +8,5 @@ export declare const handleStreamOnce: <T>(observer: Partial<Observer<T>>) => (s
 export declare const applyCallbackToMaybeAsync: <Entity, Result>(fn: (agr: Entity) => Result) => (entity: MaybeAsync<Entity>) => Subscription | Result | Promise<Result>;
 declare type MapObject = <T extends object, K extends keyof T = keyof T, V extends T[K] = T[K], NV = V>(fn: (value: V, key?: K, obj?: T) => NV, obj: T) => Record<K, NV | V>;
 export declare const mapObject: MapObject;
+export declare const deepMerge: <T, K extends keyof T>(target: Partial<T>, source: Partial<T>) => Partial<T>;
 export {};
